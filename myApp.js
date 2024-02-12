b453a09f034dfbd59da4149fe978254741224308
babotavo419 <one.faith419@gmail.com>
2024-02-12 11:56:22 -0500

    next challange

--- myApp.js
+++ myApp.js
@@ -3,9 +3,14 @@ const app = express();
 
 console.log('Hello World!');
 
+// Updated handler to serve the HTML file from a specific path
 app.get('/', function(req, res) {
-    res.send('Hello Express');
-  });
+    // Construct the path to the file based on your project structure
+    const absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+});
 
- module.exports = app;
- 
+module.exports = app;
+
+
+
