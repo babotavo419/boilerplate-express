b7fd8b289e4e5677620ce5d1f4b2c22382677aec
babotavo419 <one.faith419@gmail.com>
2024-02-12 11:32:58 -0500

    ok

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('Hello World!');
+});
 
 
 
